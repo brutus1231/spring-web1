006fa7686d5bbffd8bb6c63d64e62b923e35fbe3
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-08 12:20:47 +0100

    post button2

--- src/main/java/pl/sda/springweb1/controller/MainController.java
+++ src/main/java/pl/sda/springweb1/controller/MainController.java
@@ -3,12 +3,14 @@ package pl.sda.springweb1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     private static int COUNTER = 0;
+    private static int COUNTER2 = 0;
 
     @GetMapping("/")
     public String mainPage(@RequestParam(name = "button1", required = false) String button1, Model model) {
@@ -21,5 +23,11 @@ public class MainController {
         return "index";
     }
 
+    @PostMapping("/")
+    public String button2Clicked(@RequestParam(name = "button2") String button2, Model model) {
 
+        model.addAttribute("hello", "Hello world !!");
+        model.addAttribute("counter", "Kliknieto " + ++COUNTER2);
+        return "index";
+    }
 }
