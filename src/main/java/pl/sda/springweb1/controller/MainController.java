cbf23a36f02abcbade8ef1c7b128666f2e01d03e
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-08 11:33:11 +0100

    button click

--- src/main/java/pl/sda/springweb1/controller/MainController.java
+++ src/main/java/pl/sda/springweb1/controller/MainController.java
@@ -3,6 +3,7 @@ package pl.sda.springweb1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -11,6 +12,15 @@ public class MainController {
     public String mainPage(Model model) {
 
         model.addAttribute("hello", "Hello world !!");
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String buttonClicked(@RequestParam(name = "button1")
+                                            String button1,
+                                            Model model) {
+        int counter = 0;
+        model.addAttribute("counter", "Kliknieto " + counter);
         return "index";
     }
 }
