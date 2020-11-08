9653558946ef3e2bf511bba693d69b9433e15b66
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-08 12:15:14 +0100

    pathvariable

---
+++ src/main/java/pl/sda/springweb1/controller/NumberController.java
@@ -0,0 +1,16 @@
+package pl.sda.springweb1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class NumberController {
+
+    @GetMapping("/numbers/{id}")
+    public String getNumber(@PathVariable Integer id, Model model) {
+        model.addAttribute("number", id);
+        return "numbers";
+    }
+}
