d4c277ac0a94c9dfa1a9284754d029303251407b
zdzislaw trzaska <ztrzaska3@gmail.com>
2020-11-08 10:38:41 +0100

    Initial commit

---
+++ src/test/java/pl/sda/springweb1/SpringWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.springweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
